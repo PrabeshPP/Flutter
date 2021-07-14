33909e9b9a41f1b0aa252706795e8fa20a706b7c
Prabesh Bista <prabesh.bista1000@gmail.com>
2021-07-14 19:04:47 +0545

    model widget completed

---
+++ lib/models/catlog.dart
@@ -0,0 +1,9 @@
+class Item {
+  final String id;
+  final String name;
+  final num price;
+  final String color;
+  final String image;
+
+  Item({this.id, this.name, this.price, this.color, this.image});
+}
